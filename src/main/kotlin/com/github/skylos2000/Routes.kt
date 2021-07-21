06e85bb17bc92cff0f4704ce0ac6638d9cdac90d
Kyle Morrison <krmorrison2000@gmail.com>
2021-07-21 10:50:05 -0500

    Add new location example to the Routes.kt

--- src/main/kotlin/com/github/skylos2000/Routes.kt
+++ src/main/kotlin/com/github/skylos2000/Routes.kt
@@ -3,9 +3,14 @@ package com.github.skylos2000
 import com.github.skylos2000.plugins.getUsername
 import io.ktor.application.*
 import io.ktor.auth.*
+import io.ktor.locations.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import org.jetbrains.exposed.sql.Database
+
+// http://ipaddress/list_members_of_group/123
+@Location("/example/{groupId}")
+data class ExampleLocation(val groupId: Int)
 
 fun Application.initRoutes(db: Database) {
     routing {
@@ -19,6 +24,10 @@ fun Application.initRoutes(db: Database) {
                     "a" to 1,
                     "b" to 2
                 ))
+            }
+
+            get<ExampleLocation> {
+                call.respondText(it.groupId.toString())
             }
         }
     }
